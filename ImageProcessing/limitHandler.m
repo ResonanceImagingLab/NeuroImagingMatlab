545240e2093331f4cc9bbdd34981692f8a39cf95
Christopher Rowley <chris_rowley18@hotmail.com>
2021-09-15 16:07:18 -0400

    Update limitHandler.m
    Makes limit handler more handy with optional inputs to threshold.

--- ImageProcessing/limitHandler.m
+++ ImageProcessing/limitHandler.m
@@ -1,5 +1,19 @@
-function output = limitHandler(input)
+function output = limitHandler(input, varargin)
+% option to add in an upper and lower bound for thresholding. 
 
 input(isinf(input)) = 0;
 input(isnan(input)) = 0;
+
+if nargin == 3 
+    lowerLimit = varargin{1};
+    upperLimit = varargin{2};
+    
+    input( input < lowerLimit) = lowerLimit;
+    input( input > upperLimit) = upperLimit;
+    
+elseif nargin == 2
+    lowerLimit = varargin{1};
+    input( input < lowerLimit) = lowerLimit;
+end
+
 output = input;
