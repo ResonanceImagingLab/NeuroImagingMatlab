036f04d3b733f9b68e266471d3c2a8f056c31dda
christopherrowley <chris_rowley18@hotmail.com>
2023-09-05 08:59:29 -0400

    Update imgWithMultiSegArea2D.m

--- Display/imgWithMultiSegArea2D.m
+++ Display/imgWithMultiSegArea2D.m
@@ -17,8 +17,28 @@ if (nargin < 4)
     imgClrMap = 'gray';
 end
 
+
+
+
 boundaries = bwboundaries( seg );
-segClr = lines( length( boundaries ));
+
+if length(boundaries) == 10
+    % for the T2 maps, they get jumbled, so resort:
+    outV = [1, 10,2, 3, 9, 8, 4, 5, 7, 6 ]; % For 3D T2 array
+    %outV = [1, 10,2, 3, 9, 4, 8, 5, 7, 6 ]; % For 2D T2 array
+
+    temp ={};
+    for i = 1:10
+        temp{outV(i), 1} = boundaries{i,1};
+    end
+    boundaries = temp;
+end
+
+
+
+
+
+segClr = turbo( length( boundaries ));
 
 
 figure;
@@ -30,6 +50,7 @@ set(gcf,'Position',[100 100 1200 800])
 hold on % add the segmentation outline
 
 for k = 1:size(boundaries,1)
+    
     bound = boundaries{k};
     plot( bound(:,2), bound(:,1), 'Color', segClr(k,:), 'LineWidth', 2);
 end
